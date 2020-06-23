be0f61a2536e5c06959c69b4c71b452da5c7f58b
aleatorik <vates0328@gmail.com>
2020-06-23 23:09:43 +0900

    update: Binary conversion with recursion

--- practice/binary-conversion.js
+++ practice/binary-conversion.js
@@ -22,9 +22,25 @@ while (true) {
   //<<계속해서 무한반복 되지 않도록 종료조건 걸기>>
   if (x == 1 || x == 0) {
     //(line 22) 맨 마지막의 x 값 - 6번줄의 '1'
-    result += String(x);
+    result += String(x); // String() --string값으로 변환
     break; // 종료가 되어야 하기 때문에 break 걸어줌
   }
 }
 // console.log(result) -> 결과값은 '1101', 즉 반대로 출력됨 -- 해결방안 1) 12번 줄의 주석 해결방안 2) split(),reverse(),join() 메소드 사용
 console.log(result.split('').reverse().join('')); //split 메소드 사용 시 array 생성됨
+
+function 이진법(숫자) {
+  if (숫자 == 1 || 숫자 == 0) {
+    return String(숫자); // break역할 이기 때문에 아래 else 구문 필요없음
+  }
+  //  return String(숫자 % 2) + 이진법(Math.floor(숫자 / 2));
+
+  // console.log(이진법(11)); // 출력결과: 1101 (반대방향) -> 역순으로 바꿔야함
+  return 이진법(Math.floor(숫자 / 2)) + String(숫자 % 2);
+}
+console.log(이진법(11));
+
+이진법(11)  101 + String(1) // --최종 출력결과 : 1011 (line 32~38 결과로 1101 출력된 이유는 String()이 앞에 있었기 때문!)
+이진법(5)   10 + String(1)
+이진법(2)   1 + String(0)
+이진법(1)   1
