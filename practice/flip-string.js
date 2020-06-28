6c8e938a44c95840879a8896c0d4a8b7d7af4d68
aleatorik <vates0328@gmail.com>
2020-06-28 11:52:46 +0900

    Flip string by while loop & recursion

--- practice/flip-string.js
+++ practice/flip-string.js
@@ -20,7 +20,7 @@ console.log(result);
 // 아래는 순서대로 출력됨
 //방법1: result = '0' + result;
 //방법2: console.log(result.split('').reverse().join(''));
-
+/*
 let result = '';
 let x = 'daniel';
 while (true) {
@@ -35,3 +35,21 @@ while (true) {
   console.log(x);
 }
 console.log(result);
+*/
+function 문자열역순(문자) {
+  if (문자.length == 1) {
+    return 문자;
+  }
+  console.log(문자.slice(0, 문자.length - 1));
+  return 문자[문자.length - 1] + 문자열역순(문자.slice(0, 문자.length - 1));
+}
+
+console.log(문자열역순('daniel'));
+/*
+문자열역순('daniel') 'l' + 문자열역순('danie') -> l+einad
+문자열역순('danie')  'e' + 문자열역순('dani') -> e+inad
+문자열역순('dani')   'i' + 문자열역순('dan') -> i+nad
+문자열역순('dan')    'n' + 문자열역순('da') -> n+ad
+문자열역순('da')     'a' + 문자열역순('d') -> a+d
+문자열역순('d')      'd' 
+*/
