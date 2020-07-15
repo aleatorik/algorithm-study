39f5b2daff474dfcbdadecbcb08f909bf945b0e0
aleatorik <vates0328@gmail.com>
2020-07-15 11:27:41 +0900

    chore: Add some comments

--- practice/binary-conversion.js
+++ practice/binary-conversion.js
@@ -5,7 +5,8 @@
 // 2   2   0
 //     1       //=> 1011
 
-let result = '';
+// <<반복문으로 2진법 표현하는 방법>>
+let result = ''; // result를 문자열('')로 선언
 let x = 11;
 while (true) {
   if (x % 2 == 0) {
@@ -14,8 +15,8 @@ while (true) {
   } else {
     result += '1';
   }
-  x = Math.floor(x / 2); // -- 2로 나눴을 때 소수점 자리가 붙는 경우?
-  
+  x = Math.floor(x / 2); // -- 몫 구하기(2로 나눴을 때 나머지에 소수점 자리가 붙는 경우 버림)
+
   // Math.ceil() : 소수점 올림
   // Math.floor() : 소수점 버림
   // Math.round() : 소수점 반올림
@@ -29,7 +30,9 @@ while (true) {
 }
 // console.log(result) -> 결과값은 '1101', 즉 반대로 출력됨 -- 해결방안 1) 12번 줄의 주석 해결방안 2) split(),reverse(),join() 메소드 사용
 console.log(result.split('').reverse().join('')); //split 메소드 사용 시 array 생성됨
+//join()-- 1,0,1,1 (default는 콤마로 나누어 문자열 리턴) join('')-- 1011 (문자열로 합침)
 
+// <<재귀함수로 2진법 표현하는 방법>>
 function 이진법(숫자) {
   if (숫자 == 1 || 숫자 == 0) {
     return String(숫자); // break역할 이기 때문에 아래 else 구문 필요없음
@@ -41,7 +44,9 @@ function 이진법(숫자) {
 }
 console.log(이진법(11));
 
+/*
 이진법(11)  101 + String(1) // --최종 출력결과 : 1011 (line 32~38 결과로 1101 출력된 이유는 String()이 앞에 있었기 때문!)
 이진법(5)   10 + String(1)
 이진법(2)   1 + String(0)
 이진법(1)   1
+*/
