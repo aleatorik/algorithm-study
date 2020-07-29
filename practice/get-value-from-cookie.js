524e160c92d202b2395d599219770743925d67b6
aleatorik <vates0328@gmail.com>
2020-07-29 10:37:33 +0900

    Add another solution with For loop

--- practice/get-value-from-cookie.js
+++ practice/get-value-from-cookie.js
@@ -13,3 +13,23 @@ const getCookie = () => {
   });
   return result;
 };
+
+/* Another solution with for loop
+
+const getCookie = () => {
+    const str = document.cookie;
+    const words = str.split(";");
+    console.log("words: ",words);
+    let arrOfWecode = [];
+    for(let i = 0; i < words.length; i++) {
+      if(words[i].includes('wecode_cookie') === true) {
+        let indexOfWecode = words[i].indexOf('wecode_cookie');
+        arrOfWecode = words[i].splice(indexOfWecode);
+      }
+        let dividedArr = arrOfWecode.splice("=");
+        let result = dividedArr[1];
+    }
+    return result;
+}
+
+*/
