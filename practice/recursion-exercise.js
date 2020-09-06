07567a702a3b1215125fc9b0c12dce059a626c3c
daseulsong <vates0328@gmail.com>
2020-09-07 03:51:44 +0900

    Recursion exercise

---
+++ practice/recursion-exercise.js
@@ -0,0 +1,18 @@
+/*
+str 이라는 'string'을 넘겨주면 글자순서를 바꿔서 return해주세요.
+reverse 메서드 사용은 당연히 금지입니다!
+
+input: 'hello'
+output: 'olleh'
+
+
+
+*힌트
+아래의 코드가 어색한 것은 아니겠죠?
+(함수의 return에 string을 붙여서 사용하는 것)
+function getName(name) {
+  return name;
+}
+
+console.log(getName('김')+'님');
+*/
